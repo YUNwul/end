92e61a129f58ee67353f6374bba5dbb9f3a1df0f
YUNwul <72957551+YUNwul@users.noreply.github.com>
2020-10-24 20:45:51 +0800

    Update end.php

--- end.php
+++ end.php
@@ -5,9 +5,9 @@
 $bilibili = "bilibili1024havefun";
 
 $str = intval($_GET['id']);
-$reg = preg_match('/\d/is', $_GET['id']);
+$reg = preg_match('/\d/is', $_GET['1']);
 
-if(!is_numeric($_GET['id']) and $reg !== 1 and $str === 1){
+if(!is_numeric($_GET['1']) and $reg !== 1 and $str === 1){
 	$content = file_get_contents($_GET['url']);
 	
 	//文件路径猜解
